3eac0d97c2586a9b4508c122b3f981c847b95d61
MNEIMOH <juniornkiangmatiah>
2021-01-26 03:33:51 +0100

    Changing my home url

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
-    path('blog/', include('homepage.urls')),
+    path('', include('homepage.urls')),
     path('inventory/', include('inventory.urls')),
     path('transactions/', include('transactions.urls')),
 ]
